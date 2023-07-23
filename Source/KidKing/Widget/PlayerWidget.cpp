9c113bf45669c18388651b38bb3323ed4980bee5
Froute99 <98374874+Froute99@users.noreply.github.com>
2023-07-24 00:11:41 +0900

    Hp Sp Bar Fixed

--- Source/KidKing/Widget/PlayerWidget.cpp
+++ Source/KidKing/Widget/PlayerWidget.cpp
@@ -8,6 +8,7 @@ void UPlayerWidget::SetHealth(float Value)
 {
 	if (HealthBar)
 	{
+		UE_LOG(LogTemp, Log, TEXT("Health Bar Set"));
 		HealthBar->SetPercent(Value / MaxHealth);
 	}
 }
@@ -16,6 +17,7 @@ void UPlayerWidget::SetStamina(float Value)
 {
 	if (StaminaBar)
 	{
+		UE_LOG(LogTemp, Log, TEXT("Stamina Bar Set"));
 		StaminaBar->SetPercent(Value / MaxStamina);
 	}
 }
